95e603a104198b91a8b175da96b1dd84b63b9c3b
Brock <btibert3@gmail.com>
2025-11-21 19:40:15 +0000

    shorten retry time

--- airflow/dags/nfl-raw-pipeline.py
+++ airflow/dags/nfl-raw-pipeline.py
@@ -74,7 +74,7 @@ def nfl_raw_pipeline():
         print(f"Found {len(ids)} game_ids: {ids}")
         return ids
 
-    @task(retries=1)
+    @task(retries=1, retry_delay=timedelta(seconds=30))
     def parse_load_game_detail(game_id: str) -> dict:
         url = "https://us-central1-btibert-ba882-fall25.cloudfunctions.net/raw-parse-game"
         ctx = get_current_context()
