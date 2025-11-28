938d3d9028c42c135ca90ee089a90db9e85777fd
Brock <btibert3@gmail.com>
2025-11-28 20:04:36 +0000

    wire in LangSmith

--- functions/agent-poc/main.py
+++ functions/agent-poc/main.py
@@ -2,12 +2,27 @@ import functions_framework
 from langgraph.graph import StateGraph
 from langchain_google_vertexai import ChatVertexAI
 from langgraph.graph import StateGraph, END
+import langsmith as ls
 from typing import TypedDict, Literal
+from google.cloud import secretmanager
+import os 
 
 # --- Vertex AI setup ---
 PROJECT_ID = "btibert-ba882-fall25"   # <-- update to your project
 LOCATION = "us-central1"              # as needed
 MODEL_ID = "gemini-2.5-flash"
+SECRET_ID_LANGSMITH = 'LangSmith'   #<---------- this is the name of the secret you created
+version_id = 'latest'
+
+# instantiate the services 
+sm = secretmanager.SecretManagerServiceClient()
+name = f"projects/{PROJECT_ID}/secrets/{SECRET_ID_LANGSMITH}/versions/{version_id}"
+response = sm.access_secret_version(request={"name": name})
+ls_token = response.payload.data.decode("UTF-8")
+
+os.environ["LANGSMITH_TRACING"] = 'true'
+os.environ["LANGSMITH_API_KEY"] = ls_token
+
 
 llm = ChatVertexAI(
     model_name=MODEL_ID,
@@ -59,7 +74,8 @@ def task(request):
         # Don't try to call the LLM with None
         return ({"error": "Missing 'question' in request body"}, 400)
 
-    result = app.invoke({"question": question})
+    with ls.tracing_context(enabled=True):
+        result = app.invoke({"question": question})
 
     print("==================== result")
     print(result)
